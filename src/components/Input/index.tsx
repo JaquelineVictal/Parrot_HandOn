09a2c33fb28da8582cb71ae0219fbfcda65803c4
jvdalaglio <jvdalaglio@gmail.com>
2022-10-26 21:01:07 -0300

    Criando outros componentes

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,13 @@
 import { Inputstyle } from "./Input.style"
 
 type InputProps = {
-  name: string;
+  name:string;
   placeholder:string;
 }
 
 export default function Input(props: InputProps) {
   return (
-    <Inputstyle name={props.name} placeholder={props.placeholder}>
+    <Inputstyle>
     </Inputstyle>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import { Deslogado } from "../components/Body/Body"
 import Forms from '../components/Forms/Forms';
+import Input from "../components/Input/index"
 
 export default function Login() {
   return (
     <Deslogado>
       <Forms>
-        <p>LOGIN</p>
+        <p className="fs-3 pt-3 fw-semibold mb-5">LOGIN</p>
+        <Input name="nome" placeholder="nome"/>
       </Forms>
     </Deslogado>
   );
