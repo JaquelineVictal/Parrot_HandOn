0f73371ed7e6d556382216e0ea6af34558260e4d
Karolinegaia <drakarolinegaia@gmail.com>
2022-10-26 23:42:12 -0300

    Atualizando as Rotas

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../src/pages/Login";
-import Usuario from "../src/pages/Usuario";
+import Usuario from "./pages/Usuario";
 import Cadastro from "../src/pages/cadastro";
 import Feed from "../src/pages/Feed";
 
